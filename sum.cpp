74753a06f3309d75fe9720fa0b785583068ffe7b
chayandbsl <chayan.dbsl@gmail.com>
2023-07-31 11:16:56 +0600

    modify code

--- sum.cpp
+++ sum.cpp
@@ -1,14 +1,14 @@
-#include iostream
+#include <iostream>
 
 int sumOfNaturalNo(int n){
     int sum = 0;
-    for(int i=1; i=n; i++) {
+    for(int i=1; i<=n; i++) {
         sum += i;
     }
     return sum;
 }
 
 int main() {
-    stdcout  sumOfNaturalNo(5);
+    std::cout << sumOfNaturalNo(5);
     return 0;
 }
