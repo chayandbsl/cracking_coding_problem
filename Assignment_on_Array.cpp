db2b74378853a332feb4440a2ad5b43c7aae0608
chayandbsl <chayan.dbsl@gmail.com>
2023-08-07 15:12:43 +0600

    assignment on array

---
+++ Assignment_on_Array.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+    int prices[] = {7,5,5,3,6,4};
+    int n = std::size(prices);
+    
+    int minimumPrice = prices[0];
+    int maxProfit = 0;
+    for(int i=0; i<n; i++) {
+        if(minimumPrice > prices[i]) {
+            minimumPrice =  prices[i];
+        } else if(prices[i] - minimumPrice > maxProfit) {
+            maxProfit = prices[i] - minimumPrice;
+        }
+    }
+    std::cout << maxProfit;
+
+    return 0;
+}
+
+ Time complexity: O(n),
+ Space complexity: O(1)
