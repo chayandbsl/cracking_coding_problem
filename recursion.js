bc16a0c1bd948e54418030b0fa0aee33cad5d10c
chayandbsl <chayan.dbsl@gmail.com>
2023-08-21 15:46:36 +0600

    assignment on recursion

---
+++ recursion.js
@@ -0,0 +1,19 @@
+function sum(n1,n2) {
+    if(n2 == 0 ) {
+        return 0;
+    }
+
+    // For handling both positive value
+    if(n2>0){
+        return  n1+sum(n1,n2-1);
+    }
+    
+    // For handling negative value.
+    // if (n2 < 0) {
+    //     return -sum(n1, -n2);
+        
+    // }
+ }
+
+const result = sum(3,3);
+console.log("Result: "+ result);
